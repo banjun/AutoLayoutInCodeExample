20e7ce8de8cc36654eb264ac54e9c2e36ee328e2
banjun <banjun@gmail.com>
2014-04-07 21:14:02 +0900

    Initial Commit

---
+++ AutoLayoutInCode/BJAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  BJAppDelegate.h
+//  AutoLayoutInCode
+//
+//  Created by jun on 2014/04/07.
+//  Copyright (c) 2014年 banjun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BJAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ AutoLayoutInCode/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  AutoLayoutInCode
+//
+//  Created by jun on 2014/04/07.
+//  Copyright (c) 2014年 banjun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "BJAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([BJAppDelegate class]));
+    }
+}
